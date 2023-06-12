694cd269bfdb1eb36cf1ae0cd050b94dd0a210a9
Gerald Fuchs <Geraldf@users.noreply.github.com>
2023-06-12 20:32:09 +0200

    update story

--- src/components/Cal.stories.tsx
+++ src/components/Cal.stories.tsx
@@ -1,19 +1,19 @@
 // Button.stories.ts|tsx
 
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react';
 
 //import Cal from "./Cal";
-import Cal from "../../dist";
-import moment from "moment";
+import Cal from '../../dist';
+import moment from 'moment';
 
 const meta: Meta<typeof Cal> = {
   /* 👇 The title prop is optional.
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
-  title: "Calendar",
+  title: 'Calendar',
   component: Cal,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
 } satisfies Meta<typeof Cal>;
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -26,18 +26,18 @@ export const Default: Story = {
     startOfWeek: 1,
     calData: [
       {
-        desciption: "Test Description",
-        startDate: moment("2023-02-02", "YYYY-MM-DD").toDate(),
-        endDate: moment("2023-02-22", "YYYY-MM-DD").toDate(),
+        desciption: 'Test Description',
+        startDate: moment('2023-02-02', 'YYYY-MM-DD').toDate(),
+        endDate: moment('2023-02-22', 'YYYY-MM-DD').toDate(),
         DataType: 1,
-        dataBackgroundColor: "#eab676",
+        dataBackgroundColor: '#eab676',
       },
       {
-        desciption: "Test 22 Description",
-        startDate: moment("2023-08-13", "YYYY-MM-DD").toDate(),
-        endDate: moment("2023-09-02", "YYYY-MM-DD").toDate(),
+        desciption: 'Test 22 Description',
+        startDate: moment('2023-08-13', 'YYYY-MM-DD').toDate(),
+        endDate: moment('2023-09-02', 'YYYY-MM-DD').toDate(),
         DataType: 2,
-        dataBackgroundColor: "#521c98",
+        dataBackgroundColor: '#521c98',
       },
     ],
   },
